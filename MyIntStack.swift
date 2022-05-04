7af284b2caef422b05e1cb1d8ef3d944b59c846c
LaylaMichel1 <>
2022-05-04 01:49:23 +0000

    Added the pop method

--- MyIntStack.swift
+++ MyIntStack.swift
@@ -13,35 +13,57 @@ class MyIntStack {
         stack.append(number)
         print("Added \(number) to top of the stack.")
     }
+
+    func pop() {
+        if stack.isEmpty {
+            print("\nThe stack is empty.")
+        } else {
+            let topIndex = stack.count - 1
+            let topNum = stack[topIndex]
+            stack.remove(at: topIndex)
+            print("\nRemoved \(topNum) from the top of the stack.")
+        }
+    }
 }
 
 // Creating object
 let myStack = MyIntStack()
 
-outerloop: while true {
-    while true {
-        print("\nWhat integer do you wish to add? ", terminator: "")
-        let userNum = readLine()
+while true {
+    print("\nDo you wish to use push or pop (enter q to quit)? ", terminator: "")
+    let userMethod = readLine()
 
-        let userNumInt = Int(userNum!) ?? 23847125624345235
+    if userMethod!.uppercased == "PUSH" {
+        midloop: while true {
+            print("\nWhat integer do you wish to add? ", terminator: "")
+            let userNum = readLine()
 
-        if userNumInt == 23847125624345235 {
-            print("\nPlease enter a valid number.")
-        } else {
-            myStack.push(number: userNumInt)
+            let userNumInt = Int(userNum!) ?? 23847125624345235
 
-            while true {
-                print("\nDo you wish to continue? (y/n): ", terminator: "")
-                let userAnswer2 = readLine()
+            if userNumInt == 23847125624345235 {
+                print("\nPlease enter a valid number.")
+            } else {
+                myStack.push(number: userNumInt)
 
-                if userAnswer2!.uppercased == "Y" {
-                    break
-                } else if userAnswer2!.uppercased == "N" {
-                    break outerloop
-                } else {
-                    print("\nPlease enter either y or n.")
+                while true {
+                    print("\nDo you wish to continue? (y/n): ", terminator: "")
+                    let userAnswer2 = readLine()
+
+                    if userAnswer2!.uppercased == "Y" {
+                        break
+                    } else if userAnswer2!.uppercased == "N" {
+                        break midloop
+                    } else {
+                        print("\nPlease enter either y or n.")
+                    }
                 }
             }
         }
+    } else if userMethod!.uppercased == "POP" {
+        myStack.pop()
+    } else if userMethod!.uppercased == "Q" {
+        break
+    } else {
+        print("\nPlease enter either push, pop or q to quit.")
     }
 }
--- MyStringStack.swift
+++ MyStringStack.swift
@@ -13,29 +13,51 @@ class MyStringStack {
         stack.append(string)
         print("Added \(string) to top of the stack.")
     }
+
+    func pop() {
+        if stack.isEmpty {
+            print("\nThe stack is empty.")
+        } else {
+            let topIndex = stack.count - 1
+            let topString = stack[topIndex]
+            stack.remove(at: topIndex)
+            print("\nRemoved \(topString) from the top of the stack.")
+        }
+    }
 }
 
 // Creating object
 let myStack = MyStringStack()
 
-outerloop: while true {
-    while true {
-        print("\nWhat string do you wish to add? ", terminator: "")
-        let userString = readLine()
+while true {
+    print("\nDo you wish to use push or pop (enter q to quit)? ", terminator: "")
+    let userMethod = readLine()
 
-        myStack.push(string: userString!)
+    if userMethod!.uppercased == "PUSH" {
+        midloop: while true {
+            print("\nWhat string do you wish to add? ", terminator: "")
+            let userString = readLine()
 
-        while true {
-            print("\nDo you wish to continue? (y/n): ", terminator: "")
-            let userAnswer2 = readLine()
+            myStack.push(string: userString!)
 
-            if userAnswer2!.uppercased == "Y" {
-                break
-            } else if userAnswer2!.uppercased == "N" {
-                break outerloop
-            } else {
-                print("\nPlease enter either y or n.")
+            while true {
+                print("\nDo you wish to continue? (y/n): ", terminator: "")
+                let userAnswer2 = readLine()
+
+                if userAnswer2!.uppercased == "Y" {
+                    break
+                } else if userAnswer2!.uppercased == "N" {
+                    break midloop
+                } else {
+                    print("\nPlease enter either y or n.")
+                }
             }
         }
+    } else if userMethod!.uppercased == "POP" {
+        myStack.pop()
+    } else if userMethod!.uppercased == "Q" {
+        break
+    } else {
+        print("\nPlease enter either push, pop or q to quit.")
     }
 }
